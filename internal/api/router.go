1c22577bff8e85afaacd7540d7457794fbb321f0
Guilherme415 <guidanirs@gmail.com>
2023-08-30 23:36:13 -0300

    creating folder to dependency inside config

--- internal/api/router.go
+++ internal/api/router.go
@@ -1,9 +1,9 @@
 package api
 
 import (
-	"github.com/Guilherme415/cep-api/internal"
 	"github.com/Guilherme415/cep-api/internal/api/controller"
 	"github.com/Guilherme415/cep-api/internal/api/middleware"
+	"github.com/Guilherme415/cep-api/internal/config/dependency"
 	"github.com/gin-gonic/gin"
 )
 
@@ -11,5 +11,5 @@ func Router(e *gin.Engine) {
 	healthController := controller.NewHealthController()
 	e.GET("/health", healthController.Health)
 
-	e.Use(middleware.AuthorizationMiddleware()).GET("/cep/:cep", internal.ViacepController.GetAddressDeitalsByCEP)
+	e.Use(middleware.AuthorizationMiddleware()).GET("/cep/:cep", dependency.ViacepController.GetAddressDeitalsByCEP)
 }
--- main.go
+++ main.go
@@ -2,12 +2,12 @@ package main
 
 import (
 	"github.com/Guilherme415/cep-api/cmd"
-	"github.com/Guilherme415/cep-api/internal"
+	"github.com/Guilherme415/cep-api/internal/config/dependency"
 	"github.com/Guilherme415/cep-api/internal/config/env"
 )
 
 func main() {
 	env.LoadEnvs()
-	internal.LoadDependencies()
+	dependency.LoadDependencies()
 	cmd.StartApi()
 }
